fad3f08b5aa96c13855e74763224383e8fb9eb06
ajapha <aaronjapha@gmail.com>
2016-03-16 19:26:04 -0400

    Added publish function

---
+++ publish.js
@@ -0,0 +1,33 @@
+Channel = require('./channel');
+
+var channel = new Channel();
+
+function assertExchange(channel, microservice) {
+    return channel.assertExchange(microservice, 'topic');
+}
+
+module.exports = function(exchangeName, job, event, msgContent) {
+    channel.getChannel().then(function(ch) {
+         assertExchange(ch, exchangeName).then(function(ex) {
+            var exchange = ex.exchange;
+            console.log('exchange asserted ' + exchange);
+         
+         var routingKey = buildRoutingKey(job, event)
+         var content = new Buffer(JSON.stringify(msgContent));
+         ch.publish(exchange, routingKey, content)   
+    });
+}
+
+function buildRoutingKey(job, event) {
+    if (!job && !event) throw new Error('Empty Routing Key');
+    var routingKey = '';
+    if (job) {
+        routingKey += 'job.';
+        routingKey += (typeof job === "string" ? job : '') + '.';
+    }
+    if (event) {
+        routingKey += 'event.';
+        routingKey += (typeof job === "string" ? job : '');
+    }
+    return routingKey;
+}
